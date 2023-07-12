de4139c4cfc8832f3b5b4516145f9dfb6fd08a16
David Smith <dnsmith124@gmail.com>
2023-07-12 17:04:56 -0500

    add audio effect to blizzard spell

--- Spells/BlizzardSpellBehavior.cs
+++ Spells/BlizzardSpellBehavior.cs
@@ -6,6 +6,19 @@ public class BlizzardSpellBehavior : MonoBehaviour
 {
     public float radius = 1f;
     public float lifetime = 5f;
+    public float timeBetweenAudioClips = .5f;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        // Start the PlaySoundCoroutine
+        StartCoroutine(PlayAudioClipCoroutine());
+    }
 
     private void Update()
     {
@@ -17,5 +30,20 @@ public class BlizzardSpellBehavior : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    private IEnumerator PlayAudioClipCoroutine()
+    {
+        float timeToWait = lifetime - 1f;
+        while (timeToWait > 0)
+        {
+            audioSource.volume = Random.Range(.35f, .75f);
+            audioSource.pitch = Random.Range(.95f, 1.05f);
+            audioSource.Play();
+
+            float waitTime = Random.Range(.1f, .5f);
+            timeToWait -= waitTime;
+            yield return new WaitForSeconds(waitTime);
+        }
     }
 }
