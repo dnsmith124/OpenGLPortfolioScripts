3f02d42229342cc95a1a2b0aa55329c773484b3b
David Smith <dnsmith124@gmail.com>
2023-07-12 19:02:50 -0500

    optimize enemy loading by only setting packs active when the player is near

---
+++ AI/EnemyPackInit.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPackInit : MonoBehaviour
+{
+    public float initializationRadius = 20.0f;
+    private List<GameObject> enemies;
+    private SphereCollider sphereCollider;
+
+    void Start()
+    {
+        enemies = new List<GameObject>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform childTransform = transform.GetChild(i);
+            enemies.Add(childTransform.gameObject);
+        }
+        sphereCollider = GetComponent<SphereCollider>();
+        sphereCollider.radius = initializationRadius;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+
+        if(col.GetComponent<PlayerController>())
+        {
+            Debug.Log("Player entered");
+            foreach (GameObject enemy in enemies)
+            {
+                enemy.SetActive(true); 
+            }
+        }
+    }
+}
